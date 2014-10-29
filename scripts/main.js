61aaa361ab0992fbcd3724e44679ee58464ccc01
Victoria Nicholson <saintvictoria@gmail.com>
2014-10-29 16:50:02 -0400

    funcionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,28 @@
 
+
+var my_server = 'http://tiy-atl-fe-server.herokuapp.com/collections/vicsthings';
+
 var Task = function (options) {
   options = options || {};
   this.item = options.item || '';
-  this.status = options.status || false;
+  this.status = options.status || 'false';
   this.checked = function(){
-    this.status = true;
+    this.status = 'true';
   };
 };
 
-var task_list =[];
+var task_list;
 var task_template =$('#task_items').html();
 var rendered = _.template(task_template);
+
+$.getJSON(my_server).done(function(data){
+  task_list = data;
+
+  _.each(task_list, function(item){
+
+  })
+})
+
 var add_task = function(){
   var input = $('#task-item').val();
   if(input === ''){
@@ -20,9 +32,19 @@ var add_task = function(){
     item: input,
     status:$(this).status,
   });
-  $('#Task-List').append(rendered(task));
+
+  $.ajax({
+    type:'POST',
+    url: my_server,
+    data: task
+  }).done(function(data){
+    task_list.push(data);
+
+  $('#Task-List').append(rendered(data));
   $('#task-item').val('');
-  
+  task_list.push(task);
+});
+
 };
 
 
@@ -37,4 +59,26 @@ $('#task-item').keyup(function (e){
   if (e.keyCode == 13) {
     add_task();
   }
+});
+
+var checked;
+$('#Task-List').on('click', 'li', function
+(event){
+  event.preventDefault();
+  var myID = $(this).attr('id')
+  checked = _.findWhere(task_list, {_id:myID});
+
+  if (checked.status === 'true') {
+    checked.status = 'false';
+    $(this).removeClass('done');
+  } else {
+    checked.status = 'true';
+    $(this).addClass('done');
+  }
+
+$.ajax({
+  type: 'PUT',
+  url: my_server + "/" + checked._id,
+  data: checked
+})
 });
